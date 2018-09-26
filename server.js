20202c4fec8f7a5765a8e53d46dee4f2a0be335b
David Pham <david.pham@medela.ca>
2018-09-26 07:11:41 -0400

    Removed unnecessary dependancies from starter files. Created meal model. Defined example route.

--- server.js
+++ server.js
@@ -3,8 +3,6 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var exphbs = require("express-handlebars");
 
-var db = require("./models");
-
 var app = express();
 var PORT = process.env.PORT || 3000;
 
@@ -23,7 +21,6 @@ app.engine(
 app.set("view engine", "handlebars");
 
 // Routes
-require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
 
 var syncOptions = { force: false };
@@ -34,15 +31,13 @@ if (process.env.NODE_ENV === "test") {
   syncOptions.force = true;
 }
 
-// Starting the server, syncing our models ------------------------------------/
-db.sequelize.sync(syncOptions).then(function() {
-  app.listen(PORT, function() {
-    console.log(
-      "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
-      PORT,
-      PORT
-    );
-  });
+// Starting the server ------------------------------------/
+app.listen(PORT, function() {
+  console.log(
+    "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
+    PORT,
+    PORT
+  );
 });
 
 module.exports = app;
