a318483011ec58c89901f4016093762e2bbec5ab
David Pham <david.pham@medela.ca>
2018-09-26 08:42:19 -0400

    Front end makes an api call to back end and displays meal.

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,13 +1,22 @@
+var request = require("request");
 var Meal = require("../models/meal");
 
 module.exports = function(app) {
   // Load index page
   app.get("/", function(req, res) {
-    var meal = new Meal("Cereal", "Chicken panini", "Roast beef");
-    res.render("example", {
-      msg: "Welcome!",
-      example: meal
-    });
+    request.get(
+      "https://young-island-66909.herokuapp.com/api/getMealPlan",
+      { json: true },
+      (err, response, body) => {
+        const { breakfast, lunch, dinner } = body;
+        if (err) throw err;
+        var meal = new Meal(breakfast, lunch, dinner);
+        res.render("example", {
+          msg: "Welcome!",
+          example: meal
+        });
+      }
+    );
   });
 
   // Render 404 page for any unmatched routes
