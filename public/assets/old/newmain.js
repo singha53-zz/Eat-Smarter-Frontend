57d68a43c9c5d834238960d57ae65784108c8c49
RubyG <rubyrgill@hotmail.com>
2018-09-30 12:17:12 -0400

    Able to filter search by input

--- public/assets/old/newmain.js
+++ public/assets/old/newmain.js
@@ -7,8 +7,8 @@ $(document).ready(function () {
 });
 
 
-
 //ENTER DISH
+//------------------------------
 // Store input value 
 $("#submit").on("click", function () {
     var mealInput = $("#meal").val();
@@ -16,38 +16,56 @@ $("#submit").on("click", function () {
 });
 
 //SELECT ALLERGY
+//------------------------------
 //Store selected value 
 $("#submit").on("click", function () {
-    var allergyInput = $("#allergy").val();
-    console.log(allergyInput);
+    searchRecipes();
 })
 
-//DISPLAY RECIPES FROM YUMMILY 
-//Console.log result
-var url = "http://api.yummly.com/v1/api/recipe/recipe-id?_app_id=YOUR_ID&_app_key=YOUR_APP_KEY"
 
+
+//DISPLAY RECIPES FROM YUMMILY 
+//------------------------------
+//Console.log result
 function searchRecipes() {
-    $.get("http://api.yummly.com/v1/api/recipes?_app_id=30885432&_app_key=6eaaa7c45fffeea3f37d9adb28ad9960&q", function (response) {
+    var allergyInput = $("#allergy").val();
+    var mealInput = $("#meal").val();
+    var mealSearch = "&q=" + mealInput;
+    var tempKey = "&_app_key=6eaaa7c45fffeea3f37d9adb28ad9960&q";
+    var searchAllergy = "&allowedAllergy[]=" + allergyInput;
+
+
+    $.get("http://api.yummly.com/v1/api/recipes?_app_id=30885432" + tempKey + mealSearch + searchAllergy, function (response) {
         console.log(response);
         //saving data in array 
         var matches = response["matches"];
         for (var i = 0; i < matches.length; i++) {
             var recipeResult = matches[i];
             console.log(recipeResult)
+
+
+            var recipeName = recipeResult["sourceDisplayName"];
+            var recipeID = recipeResult["id"];
+            var recipeImage = recipeResult["smallImageUrls"];
+            var recipeIngredients = recipeResult["ingredients"];
+
+            console.log("Name:" + recipeName + " || " + "ID: " + recipeID + " || " + "Image: " + recipeImage + " || " + "Ingredients: " + recipeIngredients);
         }
     });
 };
-searchRecipes();
 
 
 //Show in html
+
+
 //On click display recipe details 
 
 
 
 
 
-    //DRAG FEATURE 
-    //Allow user to drag recipe into day of week 
+//DRAG FEATURE 
+//------------------------------
+//Allow user to drag recipe into day of week 
 
 
