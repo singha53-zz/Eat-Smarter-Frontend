11eb200d73e37e7372f23cc32ec279b1fc988c23
RubyG <rubyrgill@hotmail.com>
2018-09-30 10:14:43 -0400

    User search input stored

---
+++ public/assets/old/newmain.js
@@ -0,0 +1,24 @@
+//MAIN FUNCTIONS ON PAGE
+//------------------------------
+
+
+//ENTER DISH
+// Store input value 
+$("#submit").on("click", function () {
+    var mealInput = $("#meal").val();
+    console.log(mealInput);
+});
+
+//SELECT ALLERGY
+//Store selected value 
+
+
+//DISPLAY RECIPES FROM YUMMILY 
+//Console.log result
+//Show in html
+//On click display recipe details 
+
+//DRAG FEATURE 
+//Allow user to drag recipe into day of week 
+
+
