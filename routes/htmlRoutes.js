d101d6aedfc185b0c8f3737bc778fd4364b89a9f
rmoj <rmoj99@gmail.com>
2018-10-11 12:36:09 -0400

    Update(htmlRoutes.js): Add passport as parameter

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,19 +1,16 @@
 /* eslint-disable */
-var request = require("request");
-var Meal = require("../models/meal");
+var request = require('request');
+var Meal = require('../models/meal');
 
-module.exports = function (app) {
-
+module.exports = function(app, passport) {
   // Load index page
 
-
-  app.get("/", function (req, res) {
-    res.render("index");
+  app.get('/', function(req, res) {
+    res.render('index');
   });
 
   // Render 404 page for any unmatched routes
-  app.get("*", function (req, res) {
-    res.render("404");
+  app.get('*', function(req, res) {
+    res.render('404');
   });
-
 };
