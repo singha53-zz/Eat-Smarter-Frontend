fe9d9b23a95973e09d4d151dcc2c1637571359cb
Amrit Singh <Amrit.Singh@hli.ubc.ca>
2018-10-11 04:22:20 -0400

    update server.js

--- server.js
+++ server.js
@@ -1,17 +1,31 @@
 require("dotenv").config();
-var express = require("express");
-var bodyParser = require("body-parser");
-var exphbs = require("express-handlebars");
+var express                = require("express"),
+    bodyParser             = require("body-parser"),
+    exphbs                 = require("express-handlebars"),
+    session               = require('express-session')
+    passport               = require("passport")
 
 var app = express();
 var PORT = process.env.PORT || 3000;
 
+// For Passport
+app.use(session({ secret: 'keyboard cat',resave: true, saveUninitialized:true})); // session secret
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+
 // Middleware
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.static("public"));
 
+app.use(require("client-sessions")({
+  secret: "O Canada our home and native land.",
+  resave: false,
+  saveUninitialized: false
+}))
+
 // Handlebars
+app.set('views', './views')
 app.engine(
   "handlebars",
   exphbs({
@@ -20,17 +34,34 @@ app.engine(
 );
 app.set("view engine", "handlebars");
 
-// Routes
+//Models
+var models = require("./models");
+
+//Routes
+var authRoute = require('./routes/auth.js')(app, passport);
+require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
 
+//load passport strategies
+require('./config/passport/passport.js')(passport, models.user);
 
-// Starting the server ------------------------------------/
-app.listen(PORT, function () {
-  console.log(
-    "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
-    PORT,
-    PORT
-  );
+var syncOptions = { force: false };
+
+// If running a test, set syncOptions.force to true
+// clearing the `testdb`
+if (process.env.NODE_ENV === "development") {
+  syncOptions.force = false;
+}
+
+// Starting the server, syncing our models ------------------------------------/
+models.sequelize.sync(syncOptions).then(function() {
+  app.listen(PORT, function() {
+    console.log(
+      "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
+      PORT,
+      PORT
+    );
+  });
 });
 
 module.exports = app;
