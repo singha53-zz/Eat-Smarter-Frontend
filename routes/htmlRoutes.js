005e5113de485254ca153a6523a5d8fdc7a93d4a
Amrit Singh <Amrit.Singh@hli.ubc.ca>
2018-10-11 13:52:46 -0400

    add summary route to html file

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -5,10 +5,13 @@ var request = require("request");
 module.exports = function (app) {
 
   // Load index page
-
-
   app.get("/", function (req, res) {
     res.render("index");
+  });
+
+   // weekly summary page
+  app.get("/summary", function (req, res) {
+    res.render("summary");
   });
 
   // Render 404 page for any unmatched routes
