b9f286a7c833b47d13c9465e297e82afa5ea8b9a
rmoj <rmoj99@gmail.com>
2018-10-11 11:51:08 -0400

    Update(server.js): Imported passport, express-session, and cors libraries

--- server.js
+++ server.js
@@ -2,11 +2,17 @@ require("dotenv").config();
 var express = require("express");
 var bodyParser = require("body-parser");
 var exphbs = require("express-handlebars");
+var passport = require('passport');
+var session = require('express-session');
+var cors = require("cors");
+
+var db = require("./models");
 
 var app = express();
 var PORT = process.env.PORT || 3000;
 
 // Middleware
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(express.static("public"));
