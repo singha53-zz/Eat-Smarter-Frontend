46b92b734a54a6750f28da66ca079e53877508aa
rmoj <rmoj99@gmail.com>
2018-10-11 11:37:40 -0400

    Update(passport.js): Added signup strategy

--- config/passport.js
+++ config/passport.js
@@ -22,6 +22,65 @@ module.exports = function(passport,user){
 
   });
 
+  passport.use('local-signup', new LocalStrategy(
+
+    {           
+      usernameField : 'email',
+      passwordField : 'password',
+      passReqToCallback : true // allows us to pass back the entire request to the callback
+    },
+
+    function(req, email, password, done){
+       
+
+      var generateHash = function(password) {
+      return bCrypt.hashSync(password, bCrypt.genSaltSync(8), null);
+      };
+
+       User.findOne({where: {email:email}}).then(function(user){
+
+      if(user)
+      {
+        return done(null, false, {message : 'That email is already taken'} );
+      }
+
+      else
+      {
+        var userPassword = generateHash(password);
+        var data =
+        { email:email,
+        password:userPassword,
+        firstname: req.body.firstname,
+        lastname: req.body.lastname,
+        age: req.body.age,
+        caloricGoal: req.body.caloricGoal
+        };
+
+
+        User.create(data).then(function(newUser,created){
+          if(!newUser){
+            return done(null,false);
+          }
+
+          if(newUser){
+            return done(null,newUser);
+            
+          }
+
+
+        });
+      }
+
+
+    }); 
+
+
+
+  }
+
+
+
+  ));
 
 
 
