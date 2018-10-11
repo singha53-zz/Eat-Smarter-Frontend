0435165b52b8142414445005829fb68215e0961f
rmoj <rmoj99@gmail.com>
2018-10-11 12:47:04 -0400

    Add(htmlRoutes.js): Added get login route

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -13,6 +13,10 @@ module.exports = function(app, passport) {
     res.render('signup'); //need signup.hbs
   });
 
+  app.get('/login', function(req, res) {
+    res.render('login'); // need login.hbs
+  });
+
   // Render 404 page for any unmatched routes
   app.get('*', function(req, res) {
     res.render('404');
