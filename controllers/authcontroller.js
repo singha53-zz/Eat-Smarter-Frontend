c1b39b631b92aaa13b1df0d0f0fc58c134d889a0
rmoj <rmoj99@gmail.com>
2018-10-17 17:31:42 -0400

    Update(authcontroller.js): Transfer /api/favRecipes callback

--- controllers/authcontroller.js
+++ controllers/authcontroller.js
@@ -25,9 +25,21 @@ exports.getFaves = function(req, res) {
 }
 
 exports.newFave = function(req, res) {
- 
+    
+    console.log('server:' + JSON.stringify(req.body))
+    console.log('req.user:'+ JSON.stringify(req.user))
+
+    var recipe = req.body;
+    recipe.userId = req.user.id
+    console.log(recipe)
+    
+  db.favRecipe.create(recipe).then(function(result) {
+    console.log(result)
+      res.json(result)
+    }).catch(err =>{
+      console.log(err)
+    })
    
- 
 }
 
 exports.logout = function(req, res) {
